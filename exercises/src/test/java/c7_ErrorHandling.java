975fe2870d0dd904f1f020a4616b4ba69119e027
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2022-07-30 12:16:58 +0900

    added polling by repeat on alert

--- exercises/src/test/java/c7_ErrorHandling.java
+++ exercises/src/test/java/c7_ErrorHandling.java
@@ -206,9 +206,7 @@ public class c7_ErrorHandling extends ErrorHandlingBase {
      */
     @Test
     public void good_old_polling() {
-        //todo: change code as you need
-        Flux<String> alerts = null;
-        nodeAlerts();
+        Flux<String> alerts = nodeAlerts().repeat();
 
         //don't change below this line
         StepVerifier.create(alerts.take(2))
