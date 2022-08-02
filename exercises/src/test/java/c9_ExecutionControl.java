9b2c7009124f744ea536d41d0ff5f0f9c6395a1f
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2022-08-02 15:04:51 +0900

    executes filter, map, mapping in parallel. but wait to complete before appending to store

--- exercises/src/test/java/c9_ExecutionControl.java
+++ exercises/src/test/java/c9_ExecutionControl.java
@@ -170,11 +170,13 @@ public class c9_ExecutionControl extends ExecutionControlBase {
      */
     @Test
     public void event_processor() {
-        //todo: feel free to change code as you need
         Flux<String> eventStream = eventProcessor()
+                .parallel()
+                .runOn(Schedulers.parallel())
                 .filter(event -> event.metaData.length() > 0)
                 .doOnNext(event -> System.out.println("Mapping event: " + event.metaData))
                 .map(this::toJson)
+                .sequential()
                 .concatMap(n -> appendToStore(n).thenReturn(n));
 
         //don't change code below
