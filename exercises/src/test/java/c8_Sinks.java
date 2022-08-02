e0efa81a25449e2ad1af7109ee1fc564a9bd6823
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2022-08-02 11:16:01 +0900

    only emit elements after subscription for measurement readings

--- exercises/src/test/java/c8_Sinks.java
+++ exercises/src/test/java/c8_Sinks.java
@@ -77,11 +77,13 @@ public class c8_Sinks extends SinksBase {
      */
     @Test
     public void it_gets_crowded() {
-        //todo: feel free to change code as you need
-        Flux<Integer> measurements = null;
+        Sinks.Many<Integer> sink = Sinks.many().multicast().onBackpressureBuffer();
+        Flux<Integer> measurements = sink.asFlux();
         submitOperation(() -> {
 
             List<Integer> measures_readings = get_measures_readings(); //don't change this line
+            measures_readings.forEach(sink::tryEmitNext);
+            sink.tryEmitComplete();
         });
 
         //don't change code below
